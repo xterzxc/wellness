acff2d868691e4fa0442f1332e10fbaf1209ea08
Valera <valera.bondarev2005@gmail.com>
2023-05-17 20:38:45 +0200

    ready

--- wellness/settings.py
+++ wellness/settings.py
@@ -2,6 +2,7 @@ from pathlib import Path
 from django.utils.translation import gettext_lazy as _
 import os
 
+
 # Build paths inside the project like this: BASE_DIR / 'subdir'.
 BASE_DIR = Path(__file__).resolve().parent.parent
 
